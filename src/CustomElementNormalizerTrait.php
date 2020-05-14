9846b094626cba6d49058c78f220d75821896391
Mathias Mueller <mathias.mueller@drunomics.com>
2020-05-14 14:30:12 +0200

    Add CE normalizer trait.

---
+++ src/CustomElementNormalizerTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\custom_elements;
+
+/**
+ * Allows setter injection and simple usage of the service.
+ */
+trait CustomElementNormalizerTrait {
+
+  /**
+   * The custom element normalizer.
+   *
+   * @var \Drupal\custom_elements\CustomElementNormalizer
+   */
+  protected $customElementNormalizer;
+
+  /**
+   * Sets the custom element normalizer.
+   *
+   * @param \Drupal\custom_elements\CustomElementNormalizer $custom_element_normalizer
+   *   Custom element normalizer.
+   *
+   * @return $this
+   */
+  public function setCustomElementNormalizer(CustomElementNormalizer $custom_element_normalizer) {
+    $this->CustomElementNormalizer = $custom_element_normalizer;
+    return $this;
+  }
+
+  /**
+   * Gets the custom element normalizer.
+   *
+   * @return \Drupal\custom_elements\CustomElementNormalizer
+   *   Custom element normalizer.
+   */
+  public function getCustomElementNormalizer() {
+    if (empty($this->CustomElementNormalizer)) {
+      $this->CustomElementNormalizer = \Drupal::service('custom_elements.normalizer');
+    }
+    return $this->CustomElementNormalizer;
+  }
+
+}
