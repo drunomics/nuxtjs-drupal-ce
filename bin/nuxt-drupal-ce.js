09fdfcc59105eb79804e940cea5570697608fe1c
davision <david@artnetik.si>
2020-12-07 20:32:44 +0100

    LDP-602 - Update init script and make it executable

--- bin/nuxt-drupal-ce.js
+++ bin/nuxt-drupal-ce.js
@@ -1,47 +1,39 @@
-// const fs = require('fs')
+#!/usr/bin/env node
+const fs = require('fs')
 
-// const fileContent = `
-// <template>
-//   <main role="main" class="page">
-//     <drupal-tabs v-if="page.localTasks" :tabs="page.localTasks" />
-//     <component :is="$drupal.contentComponent()" />
-//   </main>
-// </template>
+const fileContent = `
+<template>
+  <main role="main">
+    <drupal-tabs v-if="page.localTasks" :tabs="page.localTasks" />
+    <component :is="$drupal.contentComponent()" />
+  </main>
+</template>
 
-// <script>
-// import { DrupalTabs } from 'components/Drupal'
-// import { WithAnalytics } from '@lupus/mixins/LupusPages'
-// import alterInternalLinks from '~/mixins/alterInternalLinks'
+<script>
+import { DrupalTabs } from 'components/Drupal'
 
-// export default {
-//   name: 'PageDefault',
-//   components: {
-//     DrupalTabs
-//   },
-//   mixins: [
-//     WithAnalytics,
-//     alterInternalLinks
-//   ],
-//   async asyncData ({ route, $drupal }) {
-//     const page = await $drupal.fetchPage(route.path)
-//     return { page }
-//   },
-//   head () {
-//     return {
-//       title: this.page.title,
-//       meta: this.page.metadata.meta,
-//       link: this.page.metadata.link
-//     }
-//   }
-// }
-// </script>
-// `
+export default {
+  name: 'PageDefault',
+  components: {
+    DrupalTabs
+  },
+  async asyncData ({ route, $drupal }) {
+    return { page: await $drupal.fetchPage(route.path) }
+  },
+  head () {
+    return {
+      title: this.page.title,
+      meta: this.page.metadata.meta,
+      link: this.page.metadata.link
+    }
+  }
+}
+</script>
+`
+const basePath = ''
+// The absolute path of the new file with its name
+const filepath = basePath + '_.vue'
 
-// // The absolute path of the new file with its name
-// const filepath = '../../pages/_.vue'
-
-// fs.writeFile(filepath, fileContent, (err) => {
-//   if (err) { throw err }
-// })
-
-console.log('I ran')
+fs.writeFile(filepath, fileContent, (err) => {
+  if (err) { throw err }
+})
