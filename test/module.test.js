60e77fd882d79e1cddb9fae6cdbb684bd6eaac8a
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2020-12-29 20:05:39 +0100

    test: add test case with proxy enabled

--- test/module.test.js
+++ test/module.test.js
@@ -31,3 +31,29 @@ describe('ssr-without-proxy', () => {
     expect(body).toContain('<meta data-n-head="ssr" name="title" content="Example page | Drupal 9 Custom Elements Demo" data-hid="name:title">')
   })
 })
+
+describe('ssr-with-proxy', () => {
+  setupTest({
+    testDir: __dirname,
+    fixture: '../example',
+    server: true,
+    config: {
+      'nuxtjs-drupal-ce': {
+        useProxy: 'always'
+      }
+    }
+  })
+
+  beforeAll(async () => {
+    await exec('cd example && ../bin/nuxt-drupal-ce-init.js')
+  }, 60000)
+
+  test('should render example-page', async () => {
+    setupBaseURL()
+    const { body } = await get('/example-page')
+    expect(body).toContain('This is an example-page.')
+    expect(body).toMatch(/<a .*href="\/node\/1".*>/)
+    expect(body).not.toContain('<drupal-markup>')
+    expect(body).toContain('<meta data-n-head="ssr" name="title" content="Example page | Drupal 9 Custom Elements Demo" data-hid="name:title">')
+  })
+})
