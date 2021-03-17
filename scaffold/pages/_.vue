07e3485c0a2618ad9c626eb08c93e86f8c3d23aa
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2021-03-17 18:20:20 +0100

    Update _.vue skeleton.

--- scaffold/pages/_.vue
+++ scaffold/pages/_.vue
@@ -11,18 +11,17 @@ export default {
   async asyncData ({ route, $drupal }) {
     // Do not return the data here to avoid hydrating data twice. The drupal-ce module is taking care of it already.
     await $drupal.fetchPage(route.path)
-    return { }
-  },
-  computed: {
-    page () {
-      return this.$drupal.$currentPage
-    }
   },
   head () {
     return {
       title: this.page.title,
       meta: this.page.metatags.meta,
       link: this.page.metatags.link
+    }
+  },
+  computed: {
+    page () {
+      return this.$drupal.$currentPage
     }
   }
 }
