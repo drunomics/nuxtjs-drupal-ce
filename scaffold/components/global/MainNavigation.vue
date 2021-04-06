d32c3a8eb7454820af5c747ac681653ff9c19a14
Maximilian Mikus <maximilianmikus@gmail.com>
2021-04-06 16:52:05 +0200

    LDP-697 Add main-navigation and tests for it.

---
+++ scaffold/components/global/MainNavigation.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="menu">
+    <li
+      v-for="menuItem in menus.main"
+      :key="menuItem.key"
+    >
+      <nuxt-link
+        v-if="!menuItem.external"
+        class="item"
+        :to="'/' + menuItem.alias"
+        v-text="menuItem.title"
+      />
+      <a
+        v-else
+        class="item"
+        :href="menuItem.absolute"
+        v-text="menuItem.title"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'MainNavigation',
+  computed: {
+    ...mapState('drupalCe', ['menus'])
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  .nuxt-link-active {
+    border-bottom: 2px solid #ddd;
+  }
+</style>
