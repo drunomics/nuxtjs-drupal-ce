96c06c1ba8f096edb4dd00608f1ef845359567d7
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2021-04-12 20:45:15 +0200

    Do not fetch menu by default and make tests work again.

--- scaffold/store/index.js
+++ scaffold/store/index.js
@@ -1,5 +1,7 @@
 export const actions = {
+  // Uncomment to enable menu fetching from the configured menu API endpoint.
+  // If not in use, the file and empty folder should be deleted.
   async nuxtServerInit ({ dispatch }, context) {
-    await context.$drupal.fetchMenu('main')
+    // await context.$drupal.fetchMenu('main')
   }
 }
