ecf0a874d0bd080490f73a363150aa95df56b259
Dan-Ashdown-FFT <dan.ashdown@fullfatthings.com>
2023-02-03 17:53:43 +0000

    Templates for https://www.drupal.org/project/lupus_decoupled/issues/3338911

---
+++ scaffold/layouts/layout-builder-block-preview.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isIframeResizer: false
+    }
+  },
+  head () {
+    return {
+      script: [
+        {
+          hid: 'iframeResizer',
+          src: 'https://cdn.jsdelivr.net/npm/iframe-resizer@4.3.3/js/iframeResizer.contentWindow.min.js',
+          defer: true,
+          callback: () => { this.isIframeResizer = true }
+        }
+      ]
+    }
+  }
+}
+</script>
