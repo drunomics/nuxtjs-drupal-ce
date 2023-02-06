2cd9cbfa79e485f964a94e2c3ab73435f1bc8640
TurtlBbx <turtlman@gmail.com>
2023-02-06 11:51:55 +0100

    improve: LDP-1975: Refactor for better DX. (#116)

--- src/runtime/composables/useDrupalCe.ts
+++ src/runtime/composables/useDrupalCe.ts
@@ -1,83 +1,93 @@
 import { useRuntimeConfig, useState, useFetch, navigateTo, createError, useRoute, h, resolveComponent } from '#imports'
-/**
- * Fetches page data from Drupal, handles redirects, errors and messages
- * @param path Path of the Drupal page to fetch
- * @param useFetchOptions Optional Nuxt useFetch options
- */
-export const useDrupalCeFetchPage = async (path: string, useFetchOptions = {}) => {
-  const config = useRuntimeConfig()
-  const baseURL = config.public.drupalCe.baseURL
+export const useDrupalCe = () => {
+  /**
+   * Fetches page data from Drupal, handles redirects, errors and messages
+   * @param path Path of the Drupal page to fetch
+   * @param useFetchOptions Optional Nuxt useFetch options
+   */
+  const fetchPage = async (path: string, useFetchOptions = {}) => {
+    const config = useRuntimeConfig()
+    const baseURL = config.public.drupalCe.baseURL
 
-  // Workaround for issue - useState is not available after async call (Nuxt instance unavailable)
-  const pageState = useState(`page-${path}`, () => {})
+    // Workaround for issue - useState is not available after async call (Nuxt instance unavailable)
+    const pageState = useState(`page-${path}`, () => {})
 
-  useFetchOptions.query = useFetchOptions.query ?? {}
-  useFetchOptions.key = `page-${path}`
-  useFetchOptions.baseURL = baseURL
+    useFetchOptions.query = useFetchOptions.query ?? {}
+    useFetchOptions.key = `page-${path}`
+    useFetchOptions.baseURL = baseURL
 
-  const { data: page, error } = await useFetch(path, useFetchOptions)
+    const { data: page, error } = await useFetch(path, useFetchOptions)
 
-  if (page?.value?.redirect) {
-    await navigateTo(page.value.redirect.url, {
-      external: page.value.redirect.external,
-      redirectCode: page.value.redirect.statusCode
+    if (page?.value?.redirect) {
+      await navigateTo(page.value.redirect.url, {
+        external: page.value.redirect.external,
+        redirectCode: page.value.redirect.statusCode
+      })
+      return
+    }
+
+    if (error.value && !error.value?.data?.content) {
+      throw createError({ statusCode: error.value.status, statusMessage: error.value.message, fatal: true })
+    }
+
+    if (error.value) {
+      page.value = error.value?.data
+    }
+
+    page.value?.messages && pushMessagesToState(page.value.messages)
+
+    pageState.value = page
+    return page
+  }
+
+  /**
+   * Fetches menu data from Drupal (configured by menuEndpoint option), handles errors
+   * @param name Menu name being fetched
+   */
+  const fetchMenu = async (name: string) => {
+    const config = useRuntimeConfig()
+    const baseURL = config.public.drupalCe.baseURL
+    const menuEndpoint = config.public.drupalCe.menuEndpoint
+    const menuPath = menuEndpoint.replace('$$$NAME$$$', name)
+
+    const { data: menu, error } = await useFetch(menuPath, {
+      key: `menu-${name}`,
+      baseURL
     })
-    return
+
+    if (error.value) {
+      errorMenuHandler(error)
+      return
+    }
+
+    return menu
   }
 
-  if (error.value && !error.value?.data?.content) {
-    throw createError({ statusCode: error.value.status, statusMessage: error.value.message, fatal: true })
+  /**
+   * Use messages state
+   */
+  const getMessages = () => useState('drupal-ce-messages', () => [])
+
+  /**
+   * Use page data
+   */
+  const getPage = () => useState(`page-${useRoute().path}`)
+
+  /**
+   * Render elements from page data returned from fetchPage
+   * @param customElement
+   */
+  const renderCustomElements = (customElement) => {
+    return h(resolveComponent(customElement.element), customElement)
   }
 
-  if (error.value) {
-    page.value = error.value?.data
+  return {
+    fetchPage,
+    fetchMenu,
+    getMessages,
+    getPage,
+    renderCustomElements,
   }
-
-  page.value?.messages && pushMessagesToState(page.value.messages)
-
-  pageState.value = page
-  return page
-}
-
-/**
- * Fetches menu data from Drupal (configured by menuEndpoint option), handles errors
- * @param name Menu name being fetched
- */
-export const useDrupalCeFetchMenu = async (name: string) => {
-  const config = useRuntimeConfig()
-  const baseURL = config.public.drupalCe.baseURL
-  const menuEndpoint = config.public.drupalCe.menuEndpoint
-  const menuPath = menuEndpoint.replace('$$$NAME$$$', name)
-
-  const { data: menu, error } = await useFetch(menuPath, {
-    key: `menu-${name}`,
-    baseURL
-  })
-
-  if (error.value) {
-    errorMenuHandler(error)
-    return
-  }
-
-  return menu
-}
-
-/**
- * Use messages state
- */
-export const useDrupalCeMessages = () => useState('drupal-ce-messages', () => [])
-
-/**
- * Use page data
- */
-export const useDrupalCePage = () => useState(`page-${useRoute().path}`)
-
-/**
- * Render elements from page data returned from useDrupalCeFetchPage
- * @param customElement
- */
-export const useDrupalCeRenderCustomElements = (customElement) => {
-  return h(resolveComponent(customElement.element), customElement)
 }
 
 const pushMessagesToState = (messages) => {
@@ -89,11 +99,11 @@ const pushMessagesToState = (messages) => {
   if (!messagesArray.length) {
     return
   }
-  process.client && useDrupalCeMessages().value.push(...messagesArray)
+  process.client && useDrupalCe().getMessages().value.push(...messagesArray)
 }
 
 const errorMenuHandler = (error) => {
-  process.client && useDrupalCeMessages().value.push({
+  process.client && useDrupalCe().getMessages().value.push({
     type: 'error',
     message: `Menu error: ${error.value.message}.`
   })
