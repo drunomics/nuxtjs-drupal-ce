d4513fd3441435d3b0079e4eae8c22c1d6063a10
Wolfgang Ziegler <fago@users.noreply.github.com>
2023-03-07 18:02:01 +0100

    Update nuxt.config.ts

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -7,6 +7,5 @@ export default defineNuxtConfig({
   ],
   drupalCe: {
     baseURL: process.env.DRUPAL_BASE_URL || 'https://8080-drunomics-lupusdecouple-fypxwm6rhgb.ws-eu89.gitpod.io/ce-api'
-  },
-  ssr: false
+  }
 })
