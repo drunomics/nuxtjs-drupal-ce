358f656a45747a4ef226b922e9366fb831c84811
Matic Fink <turtlman@gmail.com>
2023-05-08 15:44:17 +0200

    improve: Add a script for scaffolding default files & components.

---
+++ bin/nuxt-drupal-ce-init.cjs
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const scaffoldDir = path.join(__dirname, '/../playground')
+const target = '.'
+
+function copyFile (source, target) {
+  const targetFile = target + '/' + path.basename(source)
+
+  // If target is a directory, a new file with the same name will be created
+  if (!fs.existsSync(targetFile)) {
+    // eslint-disable-next-line no-console
+    console.log(targetFile + ' - Created.')
+    fs.writeFileSync(targetFile, fs.readFileSync(source))
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(targetFile + ' - Existing, skipped.')
+  }
+}
+
+function syncDir (directory) {
+  fs.readdirSync(scaffoldDir + '/' + directory).forEach(function (item) {
+    if (fs.lstatSync(scaffoldDir + '/' + directory + '/' + item).isDirectory()) {
+      syncDir(directory + '/' + item)
+    } else {
+      copyFile(scaffoldDir + '/' + directory + '/' + item, target + '/' + directory)
+    }
+  })
+}
+
+// Here we want to make sure our directories exist.
+fs.mkdirSync('./components/global', { recursive: true })
+fs.mkdirSync('./pages', { recursive: true })
+
+syncDir('pages')
+syncDir('components')
+copyFile(scaffoldDir + '/app.vue', target)
