9928e4fdd287b66e51dafe102bfcfd555a7e6997
Alexandru <videoloss3@gmail.com>
2023-05-12 13:20:10 +0300

    LDP-2145: Added test

---
+++ test/addRequestFormat/json.test.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath } from 'node:url'
+import { describe, it, expect } from 'vitest'
+import { setup, $fetch } from '@nuxt/test-utils'
+import DrupalCe from '../..'
+
+describe('Module addRequestFormat option set to true', async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL('../fixtures/debug', import.meta.url)),
+    nuxtConfig: {
+      modules: [
+        DrupalCe
+      ],
+      drupalCe: {
+        baseURL: '/api',
+        addRequestFormat: true
+      }
+    }
+  })
+  it('is correctly set in query', async () => {
+    const html = await $fetch('/')
+    expect(html).toContain('_format=custom_elements')
+  })
+})
