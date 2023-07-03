e8b2437e458a699bf5ce9dd7b0c7776f6a799aa5
Alexandru <videoloss3@gmail.com>
2023-07-03 15:28:15 +0300

    LDP-2185: Wrap in div and add comment

--- playground/pages/[...slug].vue
+++ playground/pages/[...slug].vue
@@ -1,16 +1,19 @@
 <template>
-  <NuxtLayout :name="page.page_layout">
-    <main>
-      <Breadcrumbs />
-      <DrupalTabs v-if="page.local_tasks" :tabs="page.local_tasks" />
-      <component :is="renderCustomElements(page.content)" />
-    </main>
-  </NuxtLayout>
+  <div>
+    <NuxtLayout :name="page.page_layout">
+      <main>
+        <Breadcrumbs />
+        <DrupalTabs v-if="page.local_tasks" :tabs="page.local_tasks" />
+        <component :is="renderCustomElements(page.content)" />
+      </main>
+    </NuxtLayout>
+  </div>
 </template>
 
 <script lang="ts" setup>
 const { fetchPage, renderCustomElements } = useDrupalCe()
 const page = await fetchPage(useRoute().path, { query: useRoute().query })
+// Set to false to support custom layouts, using <NuxtLayout> instead.
 definePageMeta({
   layout: false,
 })
