7baca04a80e59c7cc185f17327ae0f088c8146f1
Alexandru <videoloss3@gmail.com>
2023-07-03 16:41:38 +0300

    improve: LDP-2185: Add custom layout support and tests (#161)
    * improve: LDP-2185: Add custom layout support and tests
    
    * LDP-2185: Wrap in div and add comment

--- playground/pages/[...slug].vue
+++ playground/pages/[...slug].vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-    <MainNavigation />
-    <Messages />
-    <Breadcrumbs />
-    <DrupalTabs v-if="page.local_tasks" :tabs="page.local_tasks" />
-    <component :is="renderCustomElements(page.content)" />
+    <NuxtLayout :name="page.page_layout">
+      <main>
+        <Breadcrumbs />
+        <DrupalTabs v-if="page.local_tasks" :tabs="page.local_tasks" />
+        <component :is="renderCustomElements(page.content)" />
+      </main>
+    </NuxtLayout>
   </div>
 </template>
 
 <script lang="ts" setup>
 const { fetchPage, renderCustomElements } = useDrupalCe()
 const page = await fetchPage(useRoute().path, { query: useRoute().query })
+// Set to false to support custom layouts, using <NuxtLayout> instead.
+definePageMeta({
+  layout: false,
+})
 useHead({
   title: page.value.title,
   meta: page.value.metatags.meta,
---
+++ test/layout.test.ts
@@ -0,0 +1,14 @@
+import { fileURLToPath } from 'node:url'
+import { describe, it, expect } from 'vitest'
+import { setup, $fetch } from '@nuxt/test-utils'
+
+describe('Custom layouts work', async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
+    configFile: 'nuxt.config4test'
+  })
+  it('renders a page with a custom layout', async () => {
+    const html = await $fetch('/node/3')
+    expect(html).toContain('id="main-clear"')
+  })
+})
