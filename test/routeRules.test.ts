1117aad16e4e4de4c4ac31120b3e0b47f7657737
Alexandru <videoloss3@gmail.com>
2023-10-26 15:40:28 +0300

    Add test for disabled exposeAPIRouteRules

---
+++ test/routeRules.test.ts
@@ -0,0 +1,32 @@
+import { fileURLToPath } from 'node:url'
+import { describe, it, expect } from 'vitest'
+import { setup, $fetch } from '@nuxt/test-utils'
+import DrupalCe from '..'
+
+describe('Site works with exposeAPIRouteRules disabled', async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
+    nuxtConfig: {
+      modules: [
+        DrupalCe
+      ],
+      drupalCe: {
+        baseURL: '/api',
+        exposeAPIRouteRules: false
+      }
+    }
+  })
+  it('renders homepage', async () => {
+    const html = await $fetch('/')
+    expect(html).toContain('Welcome to your custom-elements enabled Drupal site')
+  })
+  it('renders menu', async () => {
+    const html = await $fetch('/')
+    expect(html).toContain('Another page')
+    expect(html).toContain('Test page')
+  })
+  it('renders test page', async () => {
+    const html = await $fetch('/node/1')
+    expect(html).toContain('Node: Test page')
+  })
+})
