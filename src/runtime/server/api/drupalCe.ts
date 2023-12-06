1bf7fc858315a52eb4e76b4c7e0d07b521682e82
Alexandru <videoloss3@gmail.com>
2023-12-06 20:31:20 +0100

    add-api-proxy: Remove console log

--- src/runtime/server/api/drupalCe.ts
+++ src/runtime/server/api/drupalCe.ts
@@ -1,5 +1,4 @@
 export default defineEventHandler(async (event) => {
-  console.log(event.context)
   const path = `/${event.context.params._.replace('api/drupal-ce', '')}`
   const drupalBaseUrl = useRuntimeConfig().public.drupalCe.baseURL
   return await proxyRequest(event, drupalBaseUrl + path)
