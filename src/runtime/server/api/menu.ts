d1ab251757fb29bbcc067af743a1451a83550756
Alexandru <videoloss3@gmail.com>
2024-01-23 13:49:03 +0200

    add-api-proxy: Refactor menu api code

--- src/runtime/server/api/menu.ts
+++ src/runtime/server/api/menu.ts
@@ -1,11 +1,12 @@
-import { defineEventHandler, proxyRequest } from 'h3'
+import { defineEventHandler, proxyRequest, getRouterParams } from 'h3'
 import { useRuntimeConfig } from '#imports'
 
 export default defineEventHandler(async (event) => {
-  const { serverDrupalBaseUrl, drupalBaseUrl, menuBaseUrl, ceApiEndpoint } = useRuntimeConfig().public.drupalCe
-  const menuUrl = menuBaseUrl || (serverDrupalBaseUrl || drupalBaseUrl) + ceApiEndpoint
-  const menu = event.context.params._
-  return await proxyRequest(event, `${menuUrl}/${menu}`, {
+  const drupalCe = useRuntimeConfig().public.drupalCe
+  const { serverDrupalBaseUrl, drupalBaseUrl, menuBaseUrl: ceMenuBaseUrl, ceApiEndpoint } = drupalCe
+  const menuBaseUrl = ceMenuBaseUrl || (serverDrupalBaseUrl || drupalBaseUrl) + ceApiEndpoint
+  const menu = getRouterParams(event)._
+  return await proxyRequest(event, `${menuBaseUrl}/${menu}`, {
     headers: {
       'Cache-Control': 'max-age=300'
     }
