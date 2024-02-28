6b5016b0288f29c9ab483c6b93454be1d5c461a8
Alexandru Teodor <videoloss3@gmail.com>
2024-02-28 19:52:48 +0200

    fix: LDP-2357: Keep session cookies when using exposeAPIRouteRules (#199)
    * fix: LDP-2357: Fix the local tasks when using exposeAPIRouteRules
    
    * LDP-2357: Add dist
    
    * Revert "LDP-2357: Add dist"
    
    This reverts commit c546916221f0ee8728b15a2c7f68b6713c4d34c8.

--- src/runtime/server/api/drupalCe.ts
+++ src/runtime/server/api/drupalCe.ts
@@ -6,5 +6,7 @@ export default defineEventHandler(async (event) => {
   const path = params ? '/' + params : ''
   const drupalCe = useRuntimeConfig().public.drupalCe
   const drupalUrl = (drupalCe.serverDrupalBaseUrl || drupalCe.drupalBaseUrl) + drupalCe.ceApiEndpoint
+  // Remove x-forwarded-proto header as it causes issues with the request.
+  delete event.req.headers['x-forwarded-proto']
   return await proxyRequest(event, drupalUrl + path)
 })
