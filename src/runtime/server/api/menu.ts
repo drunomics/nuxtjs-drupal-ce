5e9f19d8955fa81a09bfb4278cda5331ab046ff3
Alexandru <videoloss3@gmail.com>
2024-03-22 12:49:09 +0200

    LDP-2461: Improve variable name in menu route

--- src/runtime/server/api/menu.ts
+++ src/runtime/server/api/menu.ts
@@ -2,8 +2,9 @@ import { defineEventHandler, proxyRequest, getRouterParams } from 'h3'
 import { getMenuBaseUrl } from '../../composables/useDrupalCe/server'
 
 export default defineEventHandler(async (event) => {
-  const menu = getRouterParams(event)._
-  return await proxyRequest(event, `${getMenuBaseUrl()}/${menu}`, {
+  // Get the menu path along with the localization prefix.
+  const menuPath = getRouterParams(event)._
+  return await proxyRequest(event, `${getMenuBaseUrl()}/${menuPath}`, {
     headers: {
       'Cache-Control': 'max-age=300'
     }
