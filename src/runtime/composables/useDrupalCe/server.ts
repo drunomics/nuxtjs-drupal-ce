0e9c3e8cb334eb1d45e9247a9cb450aa40329bc3
Alexandru Teodor <videoloss3@gmail.com>
2024-04-15 11:53:25 +0300

    docs: LDCL-54: Add jsdocs for provided API helpers (#222)
    * fix: LDCL-54: Modify getDrupalBaseUrl to always return general base url
    
    * LDCL-54: Revert
    
    * LDCL-54: Add docs for helpers
    
    * LDCL-54: Add jsdocs

--- src/runtime/composables/useDrupalCe/server.ts
+++ src/runtime/composables/useDrupalCe/server.ts
@@ -1,10 +1,21 @@
 import { useRuntimeConfig } from '#imports'
 
+/**
+ * Returns the drupalBaseUrl.
+ * On server it returns the serverDrupalBaseUrl if set, otherwise it returns the drupalBaseUrl.
+ *
+ * @returns {string}
+ */
 export const getDrupalBaseUrl = () => {
   const config = useRuntimeConfig().public.drupalCe
   return import.meta.server && config.serverDrupalBaseUrl ? config.serverDrupalBaseUrl : config.drupalBaseUrl
 }
 
+/**
+ * Returns the menuBaseUrl if set, otherwise it returns the drupalBaseUrl + ceApiEndpoint.
+ *
+ * @returns {string}
+ */
 export const getMenuBaseUrl = () => {
   const config = useRuntimeConfig().public.drupalCe
   return config.menuBaseUrl ? config.menuBaseUrl : getDrupalBaseUrl() + config.ceApiEndpoint
