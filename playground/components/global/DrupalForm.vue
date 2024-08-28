22213f3339a1b069b5649298039b71cbf44e111e
Alexandru Teodor <videoloss3@gmail.com>
2024-08-28 08:43:54 +0300

    feat: Issue #3336148: Add support for drupal-forms (#247)
    * feat: Issue #3336148: Add support for forms
    
    * LDP-2492: Remove unnecessary if
    
    * LDP-2492: Improve redirect code
    
    * LDP-2492: Improve internal redirect code
    
    * LDP-2492: Revert redirect code
    
    * LDP-2492: Improve code and use page middleware
    
    * LDP-2492: Add server middleware and error handling
    
    * LDP-2492: Move middleware to module
    
    * LDP-2492: Include error message commit
    
    * LDP-2492: Add imports

---
+++ playground/components/global/DrupalForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <form :formId="formId" :method="method" v-bind="attributes" :action="useRoute().fullPath" class="drupal-form">
+    <slot><div v-html="content" /></slot>
+  </form>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  formId: String,
+  attributes: Object,
+  method: String,
+  content?: String,
+}>()
+</script>
