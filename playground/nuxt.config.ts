56914be7827eb5764f11bdde48ed51f5dc2d57f3
Alexandru <videoloss3@gmail.com>
2024-12-16 15:43:19 +0200

    fix: LDP-2678: Re-create package-lock and add fixes

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   modules: [
     DrupalCe,
   ],
+  compatibilityDate: '2024-12-16',
   drupalCe: {
     drupalBaseUrl: 'https://8080-drunomics-lupusdecouple-x5qe3h51r0o.ws-eu110.gitpod.io',
   },
