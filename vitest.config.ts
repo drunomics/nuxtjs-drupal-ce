a68155d89a043ebc2d8dd8c407cc9ee0086465f7
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2024-12-27 21:48:42 -0600

    do not default to nuxt environment

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,6 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
-    environment: 'nuxt',
     environmentOptions: {
       nuxt: {
         rootDir: fileURLToPath(new URL('.', import.meta.url)),
