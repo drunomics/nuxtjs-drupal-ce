a9bfdd07e0caf772964493ca483fdf22bf7bb855
Daniel Roe <daniel@roe.dev>
2024-12-29 03:53:27 +0000

    fix: augment `@nuxt/schema` rather than `nuxt/schema` (#261)

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,7 +2,7 @@ import type { ModuleOptions } from './module'
 
 // Define the type for the runtime-config,.
 // see https://nuxt.com/docs/guide/going-further/runtime-config#manually-typing-runtime-config
-declare module 'nuxt/schema' {
+declare module '@nuxt/schema' {
   interface PublicRuntimeConfig {
     drupalCe: ModuleOptions
   }
