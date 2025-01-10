720a47076ec266c811ca9c465b99524a5179fb19
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2025-01-09 22:33:13 -0600

    undo type change

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,15 +1,10 @@
-// types.ts
 import type { ModuleOptions } from './module'
 
+// Define the type for the runtime-config,.
+// see https://nuxt.com/docs/guide/going-further/runtime-config#manually-typing-runtime-config
 declare module '@nuxt/schema' {
   interface PublicRuntimeConfig {
     drupalCe: ModuleOptions
-  }
-}
-
-declare module 'nitropack' {
-  interface RouteRules {
-    drupalCeApiProxy?: boolean
   }
 }
 
