2991d8648965495cff82c4cad088451df9b228d8
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2025-01-16 22:21:46 -0600

    introduce usePageHead()

--- playground/pages/[...slug].vue
+++ playground/pages/[...slug].vue
@@ -14,24 +14,8 @@
 </template>
 
 <script lang="ts" setup>
-const { fetchPage, renderCustomElements, getPageLayout } = useDrupalCe()
+const { fetchPage, renderCustomElements, usePageHead, getPageLayout } = useDrupalCe()
 const page = await fetchPage(useRoute().path, { query: useRoute().query })
-// Set to false to support custom layouts, using <NuxtLayout> instead.
-definePageMeta({
-  layout: false,
-})
-
 const layout = getPageLayout(page)
-
-useHead({
-  title: page.value.title,
-  meta: page.value.metatags.meta,
-  link: page.value.metatags.link,
-  script: [
-    {
-      type: 'application/ld+json',
-      children: JSON.stringify(page.value.metatags.jsonld || [], null, ''),
-    },
-  ],
-})
+usePageHead(page)
 </script>
--- playground/pages/[entity]/[id]/layout-preview.client.vue
+++ playground/pages/[entity]/[id]/layout-preview.client.vue
@@ -1,40 +1,19 @@
 <script lang="ts">
-// This handles layout-builder previews. It works like the default page route,
-// but it ensures that it works with the backend login of the user previewing
-// changes.
-
-// By default, backend and frontend do not share the same cookie domain.
-// Still, we apply the backend cookie by making sure that the Drupal-API request
-// is done client-side and without API-proxy, such that the cookie is forwarded by
-// the browser. It requires the CORS helper module to be enabled.
+// Layout-builder preview route - uses client-side API requests without proxy
+// to forward cookies for authenticated preview. Requires CORS helper module.
 
 import DefaultPage from '../../[...slug].vue'
 
 export default {
   extends: DefaultPage,
   async setup() {
-    const { fetchPage, renderCustomElements, getPageLayout } = useDrupalCe()
+    const { fetchPage, renderCustomElements, usePageHead, getPageLayout } = useDrupalCe()
     const page = await fetchPage(useRoute().path, {
       query: useRoute().query,
     }, undefined, true)
 
-    definePageMeta({
-      layout: false,
-    })
-
     const layout = getPageLayout(page)
-
-    useHead({
-      title: page.value.title,
-      meta: page.value.metatags.meta,
-      link: page.value.metatags.link,
-      script: [
-        {
-          type: 'application/ld+json',
-          children: JSON.stringify(page.value.metatags.jsonld || [], null, ''),
-        },
-      ],
-    })
+    usePageHead(page)
 
     return {
       page,
