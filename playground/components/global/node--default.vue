c693524cd185916a624b4bea88836dbe195715d6
Wolfgang Ziegler <fago@users.noreply.github.com>
2025-05-08 22:37:14 +0200

    Restore original nuxt config and gitignore files

--- playground/components/global/node--default.vue
+++ playground/components/global/node--default.vue
@@ -1,28 +1,3 @@
 <template>
   <div class="node">
-    <h2 v-if="title">Node: {{ title }}</h2>
-    <slot name="image">
-      <component :is="useDrupalCe().renderCustomElements($attrs.image)" />
-    </slot>
-    <slot name="body">
-      <component :is="useDrupalCe().renderCustomElements($attrs.body)" />
-    </slot>
-    <div v-if="sections">
-      <h2>here</h2>
-      <component :is="useDrupalCe().renderCustomElements(sections)" />
-    </div>
-  </div>
-</template>
-
-<script setup lang="ts">
-defineSlots<{
-  body();
-  image();
-}>()
-defineProps<{
-  title?: string;
-  type?: string;
-  created?: number | string;
-  sections?: object[];
-}>()
-</script>
+    <h2 v-if="title">Node: {
