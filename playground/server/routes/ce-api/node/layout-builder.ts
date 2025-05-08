2b49e263dba9d39e3f8fd0d64c0e721b0c4a1f7d
Wolfgang Ziegler <fago@users.noreply.github.com>
2025-05-08 22:00:31 +0200

    Add layout builder test route

---
+++ playground/server/routes/ce-api/node/layout-builder.ts
@@ -0,0 +1,65 @@
+export default defineEventHandler(() => ({
+  title: 'Layout Builder Test',
+  messages: [],
+  breadcrumbs: [
+    {
+      frontpage: true,
+      url: '/',
+      label: 'Home'
+    },
+    {
+      url: '/node/layout-builder',
+      label: 'Layout Builder Test'
+    }
+  ],
+  content: {
+    element: 'drupal-layout',
+    layout: 'twocol',
+    settings: {
+      label: 'Two column layout',
+      column_widths: '50-50'
+    },
+    first: {
+      element: 'drupal-markup',
+      content: '<h2>First Column</h2><p>This is content in the first column of our layout builder test.</p>'
+    },
+    second: {
+      element: 'drupal-markup',
+      content: '<h2>Second Column</h2><p>This is content in the second column of our layout builder test.</p>'
+    }
+  },
+  content_format: 'json',
+  local_tasks: {
+    primary: [
+      {
+        url: '/node/layout-builder',
+        label: 'View',
+        active: true
+      },
+      {
+        url: '/node/layout-builder/layout',
+        label: 'Layout',
+        active: false
+      }
+    ]
+  },
+  metatags: {
+    meta: [
+      {
+        name: 'title',
+        content: 'Layout Builder Test | Drupal CE'
+      },
+      {
+        name: 'description',
+        content: 'A test page demonstrating layout builder functionality'
+      }
+    ],
+    link: [
+      {
+        rel: 'canonical',
+        href: 'http://localhost:3000/node/layout-builder'
+      }
+    ]
+  },
+  page_layout: 'default'
+}))
