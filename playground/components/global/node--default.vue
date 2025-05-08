867d524d72f3bc4af24160d2ecc35bcec0d6fd28
Wolfgang Ziegler <fago@users.noreply.github.com>
2025-05-08 22:38:47 +0200

    Restore original files

--- playground/components/global/node--default.vue
+++ playground/components/global/node--default.vue
@@ -1,3 +1,25 @@
 <template>
   <div class="node">
-    <h2 v-if="title">Node: {
+    <h2 v-if="title">Node: {{ title }}</h2>
+    <slot name="image">
+      <component :is="useDrupalCe().renderCustomElements($attrs.image)" />
+    </slot>
+    <slot name="body">
+      <component :is="useDrupalCe().renderCustomElements($attrs.body)" />
+    </slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineSlots<{
+  body();
+  image();
+}>()
+defineProps<{
+  title?: string;
+  type?: string;
+  created?: number | string;
+  // Layout-builder support.
+  sections?: object;
+}>()
+</script>
