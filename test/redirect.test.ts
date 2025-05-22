330b874221e56554679856691180272427d57832
Alexandru <videoloss3@gmail.com>
2025-05-22 13:20:44 +0300

    Newline

--- test/redirect.test.ts
+++ test/redirect.test.ts
@@ -2,6 +2,7 @@ import { fileURLToPath } from 'node:url'
 import { describe, it, expect } from 'vitest'
 import { setup, $fetch } from '@nuxt/test-utils'
 import { join } from 'node:path'
+
 describe('Module redirects work', async () => {
   await setup({
     rootDir: join(fileURLToPath(import.meta.url), '../../playground'),
