1bdaab514eb92d0d231d59a301f125059ba10d69
TurtlBbx <turtlman@gmail.com>
2025-10-27 09:44:03 +0100

    chore(deps-dev): Update to Nuxt 4.1.3. (#396)
    * chore(deps-dev): Update to Nuxt 4.1.3.
    
    * Fix Vitest config and organize test files in folders.
    
    * Fix jsonld.

--- test/e2e/addRequestContentFormat/markup.test.ts
+++ test/e2e/addRequestContentFormat/markup.test.ts
@@ -1,10 +1,10 @@
 import { fileURLToPath } from 'node:url'
 import { describe, it, expect } from 'vitest'
-import { setup, $fetch } from '@nuxt/test-utils'
-import DrupalCe from '../..'
+import { setup, $fetch } from '@nuxt/test-utils/e2e'
+import DrupalCe from '../../..'
 import { join } from 'node:path'
 
-describe('Module addRequestContentFormat option set to json', async () => {
+describe('Module addRequestContentFormat set to markup', async () => {
   await setup({
     rootDir: join(fileURLToPath(import.meta.url), '../../fixtures/debug'),
     nuxtConfig: {
@@ -12,16 +12,16 @@ describe('Module addRequestContentFormat option set to json', async () => {
         DrupalCe,
       ],
       drupalCe: {
-        drupalBaseUrl: 'http://127.0.0.1:3100',
+        drupalBaseUrl: 'http://127.0.0.1:3101',
         ceApiEndpoint: '/ce-api',
-        addRequestContentFormat: 'json',
+        addRequestContentFormat: 'markup',
       },
     },
-    port: 3100,
+    port: 3101,
   })
   it('is correctly set in query', async () => {
     const html = await $fetch('/')
-    expect(html).toContain('_content_format=json')
-    expect(html).not.toContain('_content_format=markup')
+    expect(html).toContain('_content_format=markup')
+    expect(html).not.toContain('_content_format=json')
   })
 })
